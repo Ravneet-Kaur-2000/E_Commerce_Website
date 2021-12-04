45d47e6f41a6972704b89d7b5684bd73b889f51a
Ravneet Kaur <ravneetkaur9978@gmail.com>
2021-12-04 18:16:53 +0530

    500 error

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,9 @@
-exports.error= (req,res,next)=>{
+exports.error400= (req,res,next)=>{
     //res.status(404).sendFile(path.join(__dirname,'views','404-error.html'));
-    res.status(404).render('404-error',{doc:'404 Error' , path:''});
+    res.status(404).render('404-error',{doc:'404 Error' , path:'',isAuthenticated:req.session.isLoggedIn});
+};
+
+exports.error500= (req,res,next)=>{
+    //res.status(404).sendFile(path.join(__dirname,'views','404-error.html'));
+    res.status(500).render('500-error',{doc:'500 Error' , path:'',isAuthenticated:req.session.isLoggedIn});
 };
